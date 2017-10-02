753248a2436411393f593d7930c8e8a938cdcf8b
manuel.landreau <thma1999>
2017-10-02 12:57:37 +0200

    mise en prod - env

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
 $dotenv->load();
 
-if (PHP_SAPI == 'cli-server' && getenv('NODE_ENV') == 'dev') {
+if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
     // something which should probably be served as a static file
     $file = __DIR__ . $_SERVER['REQUEST_URI'];
